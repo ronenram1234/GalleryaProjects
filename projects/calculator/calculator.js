cafa7f81e98539aeeb288bd67be78995ef11ecc4
Ronen Ram <ronenram1234@gmail.com>
2024-06-12 16:13:42 +0300

    update calc

--- projects/calculator/calculator.js
+++ projects/calculator/calculator.js
@@ -1,36 +1,70 @@
 "use strict";
 
-function init() {
-  let arr = document.querySelectorAll("button");
-  console.log(arr);
+let operatorSign = "";
+let calcDisplay = document.querySelector("#calcDisplay");
+let charInput = "";
+let stringInput = "";
+let currentNumber = 0;
+let firstNumber = 0;
+let pointFlag = false;
+let firstTimeKeyAfterEquel = false;
+let selectedOperated = "";
+
+// calcDisplay.innerText = "0";
+
+function initNewInput() {
+  charInput = 0;
+  currentNumber = 0;
+  stringInput = 0;
+  pointFlag = false;
+  //   calcDisplay.innerText="0"
 }
 
-
-document.addEventListener('mousedown', function(event) {
-    // Check which key was pressed
-    if (event.key === '1') {
-        // Handle key 1
-        console.log('Key 1 was pressed');
-    } else if (event.key === '2') {
-        // Handle key 2
-        console.log('Key 2 was pressed');
-    }
-    
-    console.log(event.target);
-    console.log(event.target.type.button);
-    console.log(event.target.querySelectorAll("button").dataset.tab);
-    
-    // Add similar checks for other keys (3, 4, etc.)
-});
+/*
+HL design
+- input number - add charInput if  charinput= 1-9 or "." to stringInput 
+    - pointFlag will ensure only one point added to teh number
+- pressing any operator (excluding "C" and "=")will move stringInput to firstnumber to be saved
+- pressing ("C") - clear all data - run initNewInput
+- pressing ("=") = calculate using firstNumber, currentNumber, SelectedOperated
 
 
-// function keyClicked(element){
-//     console.log('keyclicked');
-//     // console.log(element.getAttribute('data-id'));
-//     console.log(element.getAttribute('data-key'));
-//     // console.log(element.keyClicked);
-//     // console.log(element);
-    
-// }
+*/
 
-init();
+function clickCalc(event) {
+  // console.log(event);
+  charInput = event.innerText;
+  console.log(charInput);
+  //   if (isNaN(Number(charInput))) {
+
+  switch (charInput) {
+    case "0":
+    case "1":
+    case "2":
+    case "3":
+    case "4":
+    case "5":
+    case "6":
+    case "7":
+    case "8":
+    case "9":
+      stringInput += charInput;
+      calcDisplay.innerText = stringInput;
+
+      break;
+    case ".":
+      if (!pointFlag) {
+        stringInput += charInput;
+        calcDisplay.innerText = stringInput;
+        pointFlag = true;
+      }
+      break;
+    case "+":
+    case "-":
+    case "*":
+    case ".":
+    case "+":
+      firstNumber = currentNumber;
+      initNewInput();
+  }
+}
