e66aecd5638d1afb8f23b009b08e759c59f512e0
Ronen Ram <ronenram1234@gmail.com>
2024-06-14 16:22:12 +0300

    project done

--- projects/product/product.js
+++ projects/product/product.js
@@ -5,54 +5,49 @@ const itemPrice = document.querySelector("#itemPrice");
 const itemCategory = document.querySelector("#itemCategory");
 const description = document.querySelector("#description");
 const grade = document.querySelector("#grade");
-const nameP = document.querySelector("#itemName");
+
 const inStock = document.querySelector("#inStockYes");
-const tableLine = document.querySelector("#tableLine")
-const allTable = document.querySelector("#allTable")
-// const inStockNo= document.querySelector("#inStockNo")
+const tableLine = document.querySelector("#tableLine");
+const allTable = document.querySelector("#allTable");
+
+
+function createNewRow(name, price, category, description, grade, availability) {
+
+  var newRow = document.createElement("div");
+  newRow.className = "row g-1";
+  newRow.innerHTML = `
+      <div class="col col-md-2">${name}</div>
+      <div class="col col-md-2">${price}</div>
+      <div class="col col-md-2">${category}</div>
+      <div class="col col-md-2">${description}</div>
+      <div class="col col-md-2">${grade}</div>
+      <div class="col col-md-2">${availability}</div>
+    `;
+
+
+  document.querySelector("#tableLine").appendChild(newRow);
+}
 
 function addNewLine() {
   let price = Number(itemPrice.value);
-
-
   console.log(tableLine.innerHTML);
   console.log(allTable.innerHTML);
-  allTable.innerHTML+=`     <div class="row g-1" id="tableLine">
-        <div class="col col-md-2">Name</div>
-        <div class="col col-md-2">Price</div>
-        <div class="col col-md-2">Category</div>
-        <div class="col col-md-2">Description</div>
-        <div class="col col-md-2">Grade</div>
-        <div class="col col-md-2">Availability</div>
-      </div>`
-  allTable.innerHTML+=`     <div class="row g-1" id="tableLine">
-        <div class="col col-md-2">Name</div>
-        <div class="col col-md-2">Price</div>
-        <div class="col col-md-2">Category</div>
-        <div class="col col-md-2">Description</div>
-        <div class="col col-md-2">Grade</div>
-        <div class="col col-md-2">Availability</div>
-      </div>`
-  allTable.innerHTML+=`     <div class="row g-1" id="tableLine">
-        <div class="col col-md-2">Name</div>
-        <div class="col col-md-2">Price</div>
-        <div class="col col-md-2">Category</div>
-        <div class="col col-md-2">Description</div>
-        <div class="col col-md-2">Grade</div>
-        <div class="col col-md-2">Availability</div>
-      </div>`
-
 
   if (itemName.value != "" && price != 0) {
-    // console.log(tableLine.innerHTML);
-    console.log(allTable.innerHTML);
-    // allTable.innerHTML=tableLine.innerHTML+tableLine.innerHTML
-    // console.log(inStock.checked);
-    // alert("test");
+    
 
+    createNewRow(
+      itemName.value,
+      itemPrice.value,
+      itemCategory.value,
+      description.value,
+      grade.value,
+      inStock.checked
+    );
+    document.getElementById('formID').reset();
 
 
   } else {
-    // alert("Item name and Price are required");
+    alert("Item name and Price are required");
   }
 }
