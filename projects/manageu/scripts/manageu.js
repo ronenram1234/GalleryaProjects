662289b83d20ae9c56686fb62b2d1b9def8eeadc
ronen ram <ronenram1234@gmail.com>
2024-08-01 14:14:51 +0300

    completemanageu classes

--- projects/manageu/scripts/manageu.js
+++ projects/manageu/scripts/manageu.js
@@ -1,11 +1,43 @@
 "use strict";
 
 class Task {
-  constractor(description) {
-    this.id = Math.floor(Math.random() * 101 );
+  constructor(description) {
+    this.id = Math.floor(Math.random() * 101);
     this.description = this.description;
     this.complete = false;
   }
 }
 
-class TaskManager {}
+class TaskManager {
+  constructor() {
+    const taskArray = [];
+  }
+
+  addTask(desc) {
+    this.taskArray.push(new Task(desc));
+  }
+
+  findInd(id) {
+    return this.taskArray.filter((t, ind) => {
+      t.id == id;
+      console.log(pos, this.taskArray[ind]);
+      return ind;
+    });
+  }
+  deleteTask(id) {
+    let pos = this.findInd(id);
+
+    this.taskArray.splice(index, 1);
+  }
+
+  updateTaskDescription(id, desc) {
+    let pos = this.findInd(id);
+
+    this.taskArray[pos].description = desc;
+  }
+
+  completeTask(id) {
+    let pos = this.findInd(id);
+    this.taskArray[pos].complete = true;
+  }
+}
