a7ff7a7b5aac5b3cff54205750d4cb67df7426f2
ronen ram <ronenram1234@gmail.com>
2024-08-01 12:30:28 +0300

    manage u project

---
+++ projects/manageu/scripts/manageu.js
@@ -0,0 +1,11 @@
+"use strict";
+
+class Task {
+  constractor(description) {
+    this.id = Math.floor(Math.random() * 101 );
+    this.description = this.description;
+    this.complete = false;
+  }
+}
+
+class TaskManager {}
