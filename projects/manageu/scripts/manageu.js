7abc27a1953e0d7b745cfc3a5940e337bb03258a
Ronen Ram <ronenram1234@gmail.com>
2024-08-01 17:36:02 +0300

    class tested

--- projects/manageu/scripts/manageu.js
+++ projects/manageu/scripts/manageu.js
@@ -3,41 +3,57 @@
 class Task {
   constructor(description) {
     this.id = Math.floor(Math.random() * 101);
-    this.description = this.description;
+    this.description = description;
     this.complete = false;
   }
 }
 
+let taskArray = [];
 class TaskManager {
-  constructor() {
-    const taskArray = [];
-  }
+  // constructor() {
+  // }
 
   addTask(desc) {
-    this.taskArray.push(new Task(desc));
+    taskArray.push(new Task(desc));
   }
 
   findInd(id) {
-    return this.taskArray.filter((t, ind) => {
-      t.id == id;
-      console.log(pos, this.taskArray[ind]);
-      return ind;
-    });
+    return taskArray.findIndex((item) => item.id == id);
+    // return taskArray.filter((t, ind) => {
+    //   t.id == id;
+    //   console.log(ind, taskArray[ind]);
+    //   return ind;
+    // });
   }
   deleteTask(id) {
     let pos = this.findInd(id);
 
-    this.taskArray.splice(index, 1);
+    taskArray.splice(pos, 1);
   }
 
   updateTaskDescription(id, desc) {
     let pos = this.findInd(id);
 
-    this.taskArray[pos].description = desc;
+    taskArray[pos].description = desc;
   }
 
   completeTask(id) {
     let pos = this.findInd(id);
-    this.taskArray[pos].complete = true;
+    taskArray[pos].complete = true;
   }
 }
+
+
+// Test functions
+let tM = new TaskManager();
+
+tM.addTask("buy millk");
+tM.addTask("buy food for weekend");
+tM.addTask("return laundry");
+tM.addTask("clean yard");
+console.log(taskArray, taskArray[0].id);
+tM.deleteTask(taskArray[0].id);
+console.log(taskArray);
+
+tM.updateTaskDescription(taskArray[1].id,"test 12 3")
+tM.completeTask(taskArray[1].id)
